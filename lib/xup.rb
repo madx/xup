9c6cba3894586b9b3b847ed3119a491168f98d8f
madx <root@yapok.org>
2009-06-25 13:50:23 +0200

    splitting files

--- lib/xup.rb
+++ lib/xup.rb
@@ -1,42 +1,8 @@
 # coding: utf-8
 $KCODE = 'u' if RUBY_VERSION < "1.9"
 
-module Xup
+module Xup; end
 
-  class Context
-
-    attr_reader  :buffer,  :options
-
-    def initialize(options={}, &block)
-      @buffer, @options = "", options
-      instance_eval &block if block_given?
-    end
-
-    def use(*modules)
-      modules.each do |mod|
-        extend mod
-      end
-    end
-
-    def self.use(*modules)
-      modules.each do |mod|
-        send :include, mod
-      end
-    end
-
-    def build(kind = self.class, &block)
-      context = kind.new
-      context.use *(class << self; self; end).included_modules
-      concat context.tap {|c| c.instance_eval(&block) }.buffer
-    end
-
-    def build!(kind = self.class, &block)
-      concat kind.new.tap {|c| c.instance_eval(&block) }.buffer
-    end
-
-    def concat(string)
-      @buffer << string
-    end
-
-  end
+%w[context].each do |lib|
+  require File.join(File.dirname(__FILE__), 'xup', lib)
 end
---
+++ lib/xup/context.rb
@@ -0,0 +1,38 @@
+module Xup
+  class Context
+
+    attr_reader  :buffer,  :options
+
+    def initialize(options={}, &block)
+      @buffer, @options = "", options
+      instance_eval &block if block_given?
+    end
+
+    def use(*modules)
+      modules.each do |mod|
+        extend mod
+      end
+    end
+
+    def self.use(*modules)
+      modules.each do |mod|
+        send :include, mod
+      end
+    end
+
+    def build(kind = self.class, &block)
+      context = kind.new
+      context.use *(class << self; self; end).included_modules
+      concat context.tap {|c| c.instance_eval(&block) }.buffer
+    end
+
+    def build!(kind = self.class, &block)
+      concat kind.new.tap {|c| c.instance_eval(&block) }.buffer
+    end
+
+    def concat(string)
+      @buffer << string
+    end
+
+  end
+end
